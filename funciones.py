88f45f5257edcd037ad66c0647ff1f9393479b35
dhilaryw-19 <dhilaryw@gmail.com>
2026-05-15 16:48:19 -0600

    primeras funciones y validaciones para crear insertar donador

--- funciones.py
+++ funciones.py
@@ -4,3 +4,47 @@
 #Versión: 3.14.3
 
 #Importación de librerias
+import re
+from datetime import datetime
+import pickle
+
+def buscarCedula(pdonadores,pcedula):
+    """Funcionamiento: Busca una cédula dentro de la matriz.
+    Entradas:
+    pmatriz (list)
+    pcedula (str)
+    Salidas: True o False
+    """
+    for donador in pdonadores:
+        cedula = donador[1]
+        if cedula == pcedula:
+            return True
+        return False
+    
+def insertarDonador(pdonadores,pdatos):
+    cedula = pdatos[1]
+    existe = buscarCedula(pdonadores,cedula)
+    if existe == True:
+        return False
+    pdonadores.append(pdatos)
+    return True
+
+def obtenerProvincias(pcedula):
+    provincia = int(pcedula[0])
+    return provincia
+
+def validarCedula(pcedula):
+    if re.match(r"^[1-9]-\d{4}-\d{4}$",pcedula): #Valida que el formato de cedula sea #-####-#### 
+        return True
+    return False
+
+def validarFecha(pfecha):
+    try:
+        datetime.strptime(pfecha,"%d/%m/%Y") #Verifica que el str es una fecha con el formato correcto
+        return True
+    except:
+        return False
+
+#def validarCorreo():
+#def validarTelefono():
+#def validarPeso():  
--- tareaP2.py
+++ tareaP2.py
@@ -5,3 +5,37 @@
 
 #Importación de librerias
 from funciones import *
+
+#Se define variables
+tiposSangre=("O+","O-",
+             "A+","A-",
+             "B+","B-",
+             "AB+","AB-")
+
+provincias= {
+    1: "San José",
+    2: "Alajuela",
+    3: "Cartago",
+    4: "Heredia",
+    5: "Guanacaste",
+    6: "Puntarenas",
+    7: "Limon"}
+
+lugaresDonar = {
+    1:["Banco Nacional de Sangre",
+       "Hospital México",
+       "Hospital San Juan de Dios"],
+    2:["Hospital San Rafael de Alajuela",
+        "Hospital de San Ramón",
+        "Hospital del Cantón Norteño"],
+    3:["Hospital Max Peralta"],
+    4:["Hospital San Vicente de Paúl"],
+    5:["Hospital La Anexión en Nicoya",
+       "Hospital Enrique Baltodano de Liberia"],
+    6:["Hospital Monseñor Sanabria"],
+    7:["Hospital Tony Facio",
+       "Hospital de Guápiles"]}
+
+donadores= []
+
+
