8105f027252772ba8436d97dc1f80994d7736e90
arishatrsv <arishatrsva@gmail.com>
2026-05-15 22:49:29 -0600

    mejoras en vslidaciones

--- tareaP2.py
+++ tareaP2.py
@@ -12,30 +12,28 @@ lugaresDonar=crearDiccionarioLugares()
 matrizDonadores=cargarArchivo()
 
 def actualizarDonadorAux(pnombre,ptelefono,pfecha,psangre,ppeso):
-    while validarNombre(pnombre)==False:
-        print("El nombre debe contener al menos 2 caracteres.")
-        pnombre=input("Digite el nombre completo: ")
-    while validarTelefono(ptelefono)==False:
-        print("El teléfono debe tener el formato ####-#### y comenzar con 2,4,6,7,8 o 9.")
-        ptelefono=input("Digite el teléfono: ")
-    while validarFecha(pfecha)==False:
-        print("La fecha debe tener el formato dd/mm/yyyy y ser válida.")
-        pfecha=input("Digite la fecha de nacimiento: ")
-    while validarTipoSangre(psangre)==False:
-        print("El tipo de sangre debe ser O+, O-, A+, A-, B+, B-, AB+ o AB-.")
-        psangre=input("Digite el tipo de sangre: ").upper()
     while True:
+        if validarNombre(pnombre)==False:
+            print("El nombre debe contener al menos 2 caracteres.")
+            return False
+        if validarTelefono(ptelefono)==False:
+            print("El teléfono debe tener el formato ####-#### y comenzar con 2,4,6,7,8 o 9.")
+            return False
+        if validarFecha(pfecha)==False:
+            print("La fecha debe tener el formato dd/mm/yyyy y ser válida.")
+            return False
+        if validarTipoSangre(psangre)==False:
+            print("El tipo de sangre debe ser O+, O-, A+, A-, B+, B-, AB+ o AB-.")
+            return False
         try:
             ppeso=float(ppeso)
-            if validarPeso(ppeso)==True:
-                break
-            else:
+            if validarPeso(ppeso)==False:
                 print("El peso mínimo para donar es de 50 kg")
-                ppeso=input("Digite el peso: ")
+                return False
         except:
             print("El peso debe ser un valor numérico.")
-            ppeso=input("Digite el peso: ")
-    return [pnombre,ptelefono,pfecha,psangre,ppeso]
+            return False
+        return [pnombre,ptelefono,pfecha,psangre,ppeso]
 
 def opcionActualizarDonador(pposicion):
     print("Número de cédula:",matrizDonadores[pposicion][1])
@@ -45,7 +43,8 @@ def opcionActualizarDonador(pposicion):
     sangre=input("Digite el tipo de sangre: ").upper()
     peso=input("Digite el peso: ")
     datos=actualizarDonadorAux(nombre,telefono,fecha,sangre,peso)
-    return datos
+    if not datos==False:
+        return datos
         
 def menuActualizarDonador(pposicion):
     datos=opcionActualizarDonador(pposicion)
