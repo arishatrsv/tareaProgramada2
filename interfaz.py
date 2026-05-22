15501ec11d805264ca491bb23b2462bd694559e6
dhilaryw-19 <dhilaryw@gmail.com>
2026-05-22 15:23:43 -0600

    archivo interfaz.py, cree la ventana principal

---
+++ interfaz.py
@@ -0,0 +1,15 @@
+#Elaborado por: Arina Tarasova, Hilary Aguilar
+#Fecha de creación: 12/05/2026 01:30 pm
+#Última modificación:
+#Versión: 3.14.3
+
+from tkinter import *
+from funciones import *
+
+#.get() obtiene lo que el usuario escribe
+#command=, va sin paréntesis Ejecuta funciones cuando se presiona un botón:
+
+ventana = Tk() #Crea la ventana principal
+ventana.title("Banco de Sangre") #Titulo de la ventana
+ventana.geometry("700x500") #tamaño
+ventana.mainloop() #Mantiene abierta la ventana
